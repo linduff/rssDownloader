92a4af91dff2f6f9f672fb53d9d9aa96b23dbaf8
Linduff,Caleb <caleb_linduff@vanguard.com>
2023-07-20 15:45:04 +0000

    Added download for single file to test

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rssDownloader
+
+Stories:
+Download episodes
+Show download bar
+Download multiple at once
+if it fails, try again?
+Keep log of success and fails
+Make command line arg to download specific files according to date?
--- rssDownloader.py
+++ rssDownloader.py
@@ -1,14 +1,31 @@
 import sys
 import os
 import xml.etree.ElementTree as ET
+import requests
 
 if len(sys.argv) == 2 and sys.argv[1].endswith('.rss'):
     xmlParsed = ET.parse(sys.argv[1])
     root = xmlParsed.getroot()
+    episodeTitle = ''
 
-    for item in root.findall('./channel/item'):
-        for child in item:
-            if child.tag == 'title':
-                print('Downloading episode: ' + child.text)
-            if child.tag == '{http://search.yahoo.com/mrss/}content' and child.attrib['type'] == 'audio/mpeg':
-                print(child.attrib['url'])
+    # for item in root.findall('./channel/item'):
+    #     for child in item:
+    #         if child.tag == 'title':
+    #             episodeTitle = child.text.replace(" ", "_") + '.mp3'
+    #             print('Downloading episode: ' + child.text)
+                
+    #         if child.tag.endswith('content') and child.attrib['type'] == 'audio/mpeg':
+    #             epReq = requests.get(child.attrib['url'])
+    #             with open(episodeTitle, wb) as f:
+    #                 f.write(epReq.content)
+
+    items = root.findall('./channel/item')
+    for child in items[0]:
+        if child.tag == 'title':
+            episodeTitle = child.text.replace(" ", "_") + '.mp3'
+            print('Downloading episode: ' + child.text)
+            
+        if child.tag.endswith('content') and child.attrib['type'] == 'audio/mpeg':
+            epReq = requests.get(child.attrib['url'])
+            with open(episodeTitle, wb) as f:
+                f.write(epReq.content)
