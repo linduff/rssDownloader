5f884209fb3f6c80dbfc6cfa48b40f1a2988131d
Linduff,Caleb <caleb_linduff@vanguard.com>
2023-07-20 13:58:03 +0000

    Parse xml to get episode titles

--- rssDownloader.py
+++ rssDownloader.py
@@ -1,4 +1,12 @@
 import sys
 import os
-import xml
+import xml.etree.ElementTree as ET
 
+if len(sys.argv) == 2 and sys.argv[1].endswith('.rss'):
+    xmlParsed = ET.parse(sys.argv[1])
+    root = xmlParsed.getroot()
+
+    for item in root.findall('./channel/item'):
+        for child in item:
+            if child.tag == 'title':
+                print(child.text)
